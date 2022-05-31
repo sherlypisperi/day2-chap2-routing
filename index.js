c8b4ae9a49f3ea1c49d1f27cbc81a15f3c5e1c7a
sherlypisperi <sherly.pisperi1604@gmail.com>
2022-05-31 22:43:03 +0700

    day2 chap 2 routing

--- index.js
+++ index.js
@@ -5,6 +5,57 @@ const express = require('express')
 // menggunakan package express
 const app = express()
 
+const projects = [
+    {
+        projectName: 'HTML Introduction',
+        description: 'jhb',
+        duration: '7 Hari',
+        nodejs: 'nodejs',
+        reactjs: 'reactjs',
+        nextjs: 'reactjs',
+        typescript: 'typescript',
+    }
+]
+
+const month = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ]
+
+  function getDistanceTime(startDate , endDate) {
+    startDate = Number(new Date(startDate) )
+    endDate = Number(new Date(endDate))
+
+    let sumDate = endDate - startDate
+    const sec = 1000
+    const min = 60 * sec
+    const hour = 60 * min
+    const day = 24 * hour
+    const month = 30 * day
+
+    let output;
+
+    if (sumDate >= month) {
+        output = Math.floor(sumDate / month) + ' Bulan'
+    } else {
+        output = Math.floor(sumDate / day) + ' Hari'
+    }
+
+    return  output    
+}
+
+const isLogin = true
+
 
 //atur template enginge
 app.set('view engine', 'hbs')
@@ -15,11 +66,50 @@ app.use('/Public' , express.static(__dirname + '/Public'))
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/home', function (req, res) {
-    res.render('home')
+    res.render('home', {isLogin : isLogin})
 })
 
 app.get('/addproject', function (req, res) {
-    res.render('addproject')
+    // let dataProjects = projects.map(function(data){
+    //     return {
+    //         ...data,
+    //     }
+    // })
+
+    // console.log('-------',dataProjects);
+    res.render ('addproject')
+    
+})
+
+app.post('/addproject', function (req, res) {
+    let projectName = req.body.projectName
+    let startDate = req.body.startDate
+    let endDate = req.body.endDate
+    let description = req.body.description
+    let nodejs = req.body.nodejs
+    let reactjs = req.body.reactjs
+    let nextjs = req.body.nextjs
+    let typescript = req.body.typescript
+    
+
+    let project = {
+        projectName, 
+        description,
+        duration: getDistanceTime(startDate , endDate),
+        nodejs,
+        reactjs,  
+        nextjs,
+        typescript,
+    }
+
+    projects.push(project);
+    
+    res.redirect('/addproject')
+})
+
+app.get('/project-detail/:id', function (req, res) {
+    let id = req.params.id
+    res.render('addproject' , {id:id})
 })
 
 app.get('/contact-me', function (req, res) {
@@ -30,6 +120,10 @@ app.get('/project-detail', function (req, res) {
     res.render('project-detail')
 })
 
+
+// app.post('/home' , function (req,res) {
+//     res.render('index')
+// })
    
 
 const port = 5000
